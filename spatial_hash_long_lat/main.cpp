c7878de280cb4fb64d723fdad7733e959865f891
David Carpenter <davidc538@hotmail.com>
2016-10-16 10:15:22 -0400

    Updated test.

--- spatial_hash_long_lat/main.cpp
+++ spatial_hash_long_lat/main.cpp
@@ -8,7 +8,10 @@ int main(int argc, char** argv)
 
 	std::cout << sh.ComputeHash(1.9999, 2.0001) << std::endl;
 
-	auto strs = sh.GetHashesForCellsInRange(2.0f, 2.0f, 2500.0f);
+	auto strs = sh.GetHashesForCellsInRange(2.0f, 2.0f, 300.0f);
+
+	for (const auto& str : strs)
+		std::cout << str << "|";
 
 	std::cout << "end." << std::endl;
 	std::cin.get();
